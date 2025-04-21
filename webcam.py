4a75f24b0d74f45e26afe36611a911d8733bfc0d
Trishu Patel <pateltrishu28@gmail.com>
2025-04-21 16:36:34 -0400

    feat: Add ensemble support

--- webcam.py
+++ webcam.py
@@ -6,19 +6,36 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 import torchvision.transforms.v2 as v2
 import PIL.Image
+import argparse
 
 MIN_AREA = 2000
 OUTPUT_WIDTH, OUTPUT_HEIGHT = 450, 635
 CLASSES = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "Face/10"]
-MODEL_PATH = os.path.join("models", "test_deck2+epoch_3.pt")
+MODEL_PATHS = [
+    os.path.join("models", "test_deck1+epoch_3.pt"),
+    os.path.join("models", "test_deck2+epoch_3.pt"),
+    os.path.join("models", "test_deck4+epoch_3.pt"),
+]
+TRANSFORMATIONS = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        v2.GaussianNoise(sigma=0.02),
+        transforms.GaussianBlur(7),
+    ]
+)
+COLORS = [
+    (255, 0, 0),
+    (0, 255, 0),
+    (0, 0, 255),
+    (0, 255, 255),
+]
+
 
 def image_preprocessing(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
-    _, filtered = cv2.threshold(
-        blurred, 175, 255, cv2.THRESH_BINARY_INV
-    )
+    _, filtered = cv2.threshold(blurred, 175, 255, cv2.THRESH_BINARY_INV)
 
     kernel = np.ones((3, 3), np.uint8)
     img_erosion = cv2.erode(filtered, kernel, iterations=1)
@@ -49,7 +66,11 @@ def order_points(pts):
     return rect
 
 
-def extract_card(image, box):
+def extract_card(image, contour):
+    rect = cv2.minAreaRect(contour)
+    box = cv2.boxPoints(rect)
+    box = order_points(box)
+
     output_box = np.float32(
         [[0, 0], [OUTPUT_WIDTH, 0], [0, OUTPUT_HEIGHT], [OUTPUT_WIDTH, OUTPUT_HEIGHT]]
     )
@@ -69,26 +90,53 @@ def extract_card(image, box):
     gray = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
     resized = cv2.resize(gray, (90, 126))
 
-    return resized
+    transformed = TRANSFORMATIONS(resized)
+
+    return transformed, box
+
+
+def make_prediction(card, model):
+    logits = model(card.unsqueeze(0))
+    probs = nn.Softmax(dim=1)(logits)
+    predicted_idx = torch.argmax(probs, dim=1).item()
+
+    return CLASSES[predicted_idx]
+
+
+def add_label(image, prediction, position, color):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.75
+    thickness = 2
+
+    cv2.putText(
+        image,
+        prediction,
+        np.intp(position),
+        font,
+        font_scale,
+        color,
+        thickness,
+        cv2.LINE_AA,
+    )
 
 
 def main():
-    cap = cv2.VideoCapture(1) 
+    cap = cv2.VideoCapture(1)
 
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
 
-    model: torch.nn.Module = torch.load(MODEL_PATH, weights_only=False)
-    model.eval()
+    models = [torch.load(path, weights_only=False).eval() for path in MODEL_PATHS]
 
-    transform = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            v2.GaussianNoise(sigma=0.02),
-            transforms.GaussianBlur(7),
-        ]
+    parser = argparse.ArgumentParser(
+        prog="JackGPT Webcam",
+        description="This script uses the webcam to classify playing cards",
     )
+
+    parser.add_argument("--debug", action="store_true", help="Show debug options")
+
+    args = parser.parse_args()
 
     while True:
         ret, image = cap.read()
@@ -100,36 +148,18 @@ def main():
         contours = get_contours(processed_image)
 
         for contour in contours:
-            rect = cv2.minAreaRect(contour)
-            box = cv2.boxPoints(rect)
-            box = order_points(box)
+            card, box = extract_card(image, contour)
 
-            card = extract_card(image, box)
-            card = transform(card)
+            predictions = np.array([make_prediction(card, model) for model in models])
 
-            with torch.inference_mode():
-                logits = model(card.unsqueeze(0))
-                probs = nn.Softmax(dim=1)(logits)
-                predicted_idx = torch.argmax(probs, dim=1).item()
+            if args.debug:
+                for index, (prediction, color) in enumerate(zip(predictions, COLORS)):
+                    add_label(image, prediction, box[index], color)
 
-            # Font settings
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            font_scale = 0.75
-            color = (0, 255, 255)
-            thickness = 2
-
+            values, counts = np.unique(predictions, return_counts=True)
+            final_prediction = values[np.argmax(counts)]
             center = int(np.mean(box[:, 0])) - 10, int(np.mean(box[:, 1]))
-
-            cv2.putText(
-                image,
-                CLASSES[predicted_idx],
-                center,
-                font,
-                font_scale,
-                color,
-                thickness,
-                cv2.LINE_AA,
-            )
+            add_label(image, final_prediction, center, COLORS[-1])
 
         cv2.imshow("IP Camera Feed", image)
 
