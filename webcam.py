2fa9cdd4d96a4a6cedea2bbedee1a4e9a42f7762
Trishu Patel <141292044+Trishu-Patel@users.noreply.github.com>
2025-04-26 18:05:21 -0400

    feat: Add webcam pipline (#6)
    * feat: Connect model to webcam
    
    * feat: Use webcam
    
    * feat: Add ensemble support
    
    * feat: Connect model to webcam
    
    * feat: Use webcam
    
    * feat: Add ensemble support
    
    * feat: Support 13 and 52 class models
    
    * fix: Correct model index to label mapping
    
    * feat: Update font sizes
    
    * avoided hardcoded video device
    
    ---------
    
    Co-authored-by: Evan Abbott <107959883+EvanA4@users.noreply.github.com>

--- webcam.py
+++ webcam.py
@@ -5,17 +5,13 @@ import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 import torchvision.transforms.v2 as v2
-import PIL.Image
 import argparse
+
+export OPENCV_LOG_LEVEL=OFF
 
 MIN_AREA = 2000
 OUTPUT_WIDTH, OUTPUT_HEIGHT = 450, 635
 CLASSES = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "Face/10"]
-MODEL_PATHS = [
-    os.path.join("models", "test_deck1+epoch_3.pt"),
-    os.path.join("models", "test_deck2+epoch_3.pt"),
-    os.path.join("models", "test_deck4+epoch_3.pt"),
-]
 TRANSFORMATIONS = transforms.Compose(
     [
         transforms.ToTensor(),
@@ -29,6 +25,31 @@ COLORS = [
     (0, 0, 255),
     (0, 255, 255),
 ]
+
+
+def get_model_path(num_of_classes):
+    total_num_models = 5 if num_of_classes == 52 else 3
+
+    return [
+        os.path.join("models", f"class_{num_of_classes}_model_{num_of_models}.pt")
+        for num_of_models in range(1, total_num_models + 1)
+    ]
+
+
+def get_class_name(num_of_classes, prediction_index):
+    classes_10 = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "Face/10"]
+    classes_13 = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    suits = ["C", "D", "H", "S"]
+
+    if num_of_classes == 10:
+        return classes_10[prediction_index]
+    if num_of_classes == 13:
+        return classes_13[prediction_index]
+    if num_of_classes == 52:
+        rank = classes_13[prediction_index // 4]
+        suit = suits[prediction_index % 4]
+
+        return f"{suit}{rank}"
 
 
 def image_preprocessing(image):
@@ -95,17 +116,16 @@ def extract_card(image, contour):
     return transformed, box
 
 
-def make_prediction(card, model):
+def make_prediction(card, model, num_of_classes):
     logits = model(card.unsqueeze(0))
     probs = nn.Softmax(dim=1)(logits)
     predicted_idx = torch.argmax(probs, dim=1).item()
 
-    return CLASSES[predicted_idx]
+    return get_class_name(num_of_classes, predicted_idx)
 
 
-def add_label(image, prediction, position, color):
+def add_label(image, prediction, position, color, font_scale = 0.5):
     font = cv2.FONT_HERSHEY_SIMPLEX
-    font_scale = 0.75
     thickness = 2
 
     cv2.putText(
@@ -120,14 +140,56 @@ def add_label(image, prediction, position, color):
     )
 
 
+def get_chosen_idx() -> int:
+    # os.environ["OPENCV_LOG_LEVEL"]="SILENT"
+    # os.environ['OPENCV_FFMPEG_LOGLEVEL'] = "-8"
+
+    # get the available video capture indices
+    print("Searching for valid video devices... expect 1 error", flush=True)
+    video_idxs = []
+    cur_idx = 0
+    found_invalid = False
+    while not found_invalid:
+        tmp_cap = cv2.VideoCapture(cur_idx)
+        if not tmp_cap.isOpened():
+            found_invalid = True
+            break
+        print(f"Found device #{cur_idx}", flush=True)
+        tmp_cap.release()
+        video_idxs.append(cur_idx)
+        cur_idx += 1
+
+    # if none available, error
+    if len(video_idxs) == 0:
+        print("Error: no available cameras.", flush=True)
+        exit(1)
+
+    # default if there's only one
+    elif len(video_idxs) == 1:
+        chosen_idx = video_idxs[0]
+        print(f"Only available video device is #{chosen_idx}.", flush=True)
+
+    # else, have user decide
+    else:
+        while chosen_idx == -1:
+            try:
+                chosen_idx = int(input(f"Which camera would you like to use [0-{video_idxs[-1]}]? "))
+                if chosen_idx > video_idxs[-1] or chosen_idx < 0:
+                    chosen_idx = -1
+            except ValueError:
+                continue
+            except KeyboardInterrupt:
+                exit()
+
+    return chosen_idx
+
 def main():
-    cap = cv2.VideoCapture(1)
+    chosen_idx = get_chosen_idx()
+    cap = cv2.VideoCapture(chosen_idx)
 
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
-
-    models = [torch.load(path, weights_only=False).eval() for path in MODEL_PATHS]
 
     parser = argparse.ArgumentParser(
         prog="JackGPT Webcam",
@@ -135,8 +197,14 @@ def main():
     )
 
     parser.add_argument("--debug", action="store_true", help="Show debug options")
+    parser.add_argument("--classes", type=int, help="The number of output classes", required=True)
 
     args = parser.parse_args()
+
+    models = [
+        torch.load(path, weights_only=False).eval()
+        for path in get_model_path(args.classes)
+    ]
 
     while True:
         ret, image = cap.read()
@@ -150,18 +218,18 @@ def main():
         for contour in contours:
             card, box = extract_card(image, contour)
 
-            predictions = np.array([make_prediction(card, model) for model in models])
+            predictions = np.array([make_prediction(card, model, args.classes) for model in models])
 
             if args.debug:
                 for index, (prediction, color) in enumerate(zip(predictions, COLORS)):
-                    add_label(image, prediction, box[index], color)
+                    add_label(image, prediction, box[index % 4], color)
 
             values, counts = np.unique(predictions, return_counts=True)
             final_prediction = values[np.argmax(counts)]
             center = int(np.mean(box[:, 0])) - 10, int(np.mean(box[:, 1]))
-            add_label(image, final_prediction, center, COLORS[-1])
+            add_label(image, final_prediction, center, COLORS[-1], font_scale=1)
 
-        cv2.imshow("IP Camera Feed", image)
+        cv2.imshow("IP Camera Feed", cv2.resize(image, (1280, 960)))
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
